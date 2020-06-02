67792c950c7b04f4e2b47c3aaf615c38a2dd6fff
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-06-02 18:39:35 +0300

    Adding Fragment Java and XML files

--- app/src/main/java/com/example/learnnavdrawerfragment/MainActivity.java
+++ app/src/main/java/com/example/learnnavdrawerfragment/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.learnnavdrawerfragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 //    Basic Variables for the Navigation Drawer
     DrawerLayout mDrawerLayout;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        The Custom Toolbar created
         mToolbar = findViewById(R.id.nav_drawer_toolbar);
         setSupportActionBar(mToolbar);
 
@@ -29,12 +33,34 @@ public class MainActivity extends AppCompatActivity {
         mNavigationView = findViewById(R.id.navigationView);
 
 //        Add the Navigation Drawer(DrawerLayout and ToolBar) to this Activity
-        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.open, R.string.close);
 //        Add a listener for this toggle
         mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
 //        Enable the Hamburger Sign
         mActionBarDrawerToggle.setDrawerIndicatorEnabled(true);
 //        Sync the State of the navigation drawer - is it open or closed
         mActionBarDrawerToggle.syncState();
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        This listens to the Menu Items that Appear on the Navigation Drawer
+        int itemId = menuItem.getItemId();
+        switch (itemId){
+            case(R.id.menu_home):
+                break;
+            case(R.id.menu_new):
+                break;
+            case(R.id.menu_share):
+                break;
+            case(R.id.menu_email):
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/learnnavdrawerfragment/SecondFragment.java
@@ -0,0 +1,7 @@
+package com.example.learnnavdrawerfragment;
+
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+//    All Fragment Java Classes require their Accompanying XML
+}
