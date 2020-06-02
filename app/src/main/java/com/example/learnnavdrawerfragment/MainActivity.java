acb17d32cd4476af8325549cbc90b266f408cde5
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-06-02 12:50:43 +0300

    Adding the Nav Drawer to the MainActivity

--- app/src/main/java/com/example/learnnavdrawerfragment/MainActivity.java
+++ app/src/main/java/com/example/learnnavdrawerfragment/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.learnnavdrawerfragment;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+//    Basic Variables for the Navigation Drawer
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mActionBarDrawerToggle;
+    Toolbar mToolbar;
+    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(mToolbar);
+
+//        References to Navigation Drawer variables
+        mDrawerLayout = findViewById(R.id.nav_drawer_main_activity);
+        mToolbar = findViewById(R.id.nav_drawer_toolbar);
+        mNavigationView = findViewById(R.id.navigationView);
+
+//        Add the Navigation Drawer(DrawerLayout and ToolBar) to this Activity
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
+//        Add a listener for this toggle
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+//        Enable the Hamburger Sign
+        mActionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+//        Sync the State of the navigation drawer - is it open or closed
+        mActionBarDrawerToggle.syncState();
     }
 }
